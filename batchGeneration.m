6e83b7fbce5265bcefaea605e9da583ae3656f20
elFeliz <felix.kunze@t-online.de>
2025-04-14 15:46:42 +0200

    feat(progressBar): implement command-line progress bar and update batch generation to use it

--- batchGeneration.m
+++ batchGeneration.m
@@ -1,13 +1,12 @@
 outputFolder = './output/';
+nHouses = 1000; % Number of days to simulate
 
-nDays = 1400; % Number of days to simulate
-
-for i = 1:nDays
-    fprintf('Generating data: %d/%d [%.1f%%]\r', i, nDays, (i/nDays)*100);
+pb = CmdLineProgressBar('Generating data...');
+for i = 1:nHouses
+    pb.print(i, nHouses);
     MAIN_STREaM
     outputTrajectoryTable = struct2table(outputTrajectory);
-    writetable(outputTrajectoryTable, sprintf('%sDay_%d_10s_trajectory.csv', outputFolder, i))
-    writetable(statistics, sprintf('%sDay_%d_10s_statistics.csv', outputFolder, i))
+    writetable(outputTrajectoryTable, sprintf('%sHouse_%d_10s_trajectory.csv', outputFolder, i))
+    writetable(statistics, sprintf('%sHouse_%d_10s_statistics.csv', outputFolder, i))
 end
-fprintf('\n'); % Add new line at the end
 
